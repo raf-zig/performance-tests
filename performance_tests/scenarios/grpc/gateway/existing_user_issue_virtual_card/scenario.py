774435402babca9f2a99ba2b371ebe47da3683a4
raf-zig <ziravin#yandex.ru>
2025-12-30 00:41:47 +0500

    Практика нагрузочного тестирования. Часть 3. Практика: Выпуск виртуальной карты существующим пользователем

--- performance_tests/scenarios/grpc/gateway/existing_user_issue_virtual_card/scenario.py
+++ performance_tests/scenarios/grpc/gateway/existing_user_issue_virtual_card/scenario.py
@@ -1,33 +1,39 @@
 from locust import task, events
 from locust.env import Environment
 
-from performance_tests.clients.http.gateway.locust import GatewayHTTPTaskSet
+from performance_tests.clients.grpc.gateway.locust import GatewayGRPCTaskSet
 from performance_tests.seeds.scenarios.existing_user_issue_virtual_card import ExistingUserIssueVirtualCardSeedsScenario
 from performance_tests.seeds.schema.result import SeedUserResult
 from performance_tests.tools.locust.user import LocustBaseUser
 
 
-# Хук инициализации — вызывается перед началом запуска нагрузки
+# Этот хук выполняется один раз при инициализации теста (до старта пользователей).
+# Мы используем его, чтобы заранее прогнать сидинг и загрузить пользователей в память.
 @events.init.add_listener
 def init(environment: Environment, **kwargs):
-    # Выполняем сидинг
+    # Создаем экземпляр сидинг-сценария
     seeds_scenario = ExistingUserIssueVirtualCardSeedsScenario()
-    seeds_scenario.build()  # создаём пользователей, счета, карты и операции
 
-    # Загружаем результат сидинга (из файла JSON)
+    # Выполняем генерацию данных, если они ещё не созданы
+    seeds_scenario.build()
+
+    # Загружаем сгенерированных пользователей в окружение Locust
     environment.seeds = seeds_scenario.load()
 
-class IssueVirtualCardTaskSet(GatewayHTTPTaskSet):
-    seed_user: SeedUserResult  # Типизированная ссылка на данные из сидинга
+class IssueVirtualCardTaskSet(GatewayGRPCTaskSet):
+    # Типизируем объект пользователя из сидинга
+    seed_user: SeedUserResult
 
+    # Метод вызывается при запуске каждой сессии пользователя (до начала задач)
     def on_start(self) -> None:
         super().on_start()
-        # Получаем случайного пользователя из подготовленного списка
+
+        # Получаем пользователя
         self.seed_user = self.user.environment.seeds.get_random_user()
 
     @task(2)
     def get_accounts(self):
-        # Получаем список счетов пользователя
+        # Запрашиваем список счетов
         self.accounts_gateway_client.get_accounts(user_id=self.seed_user.user_id)
 
     @task(1)
@@ -35,7 +41,5 @@ class IssueVirtualCardTaskSet(GatewayHTTPTaskSet):
         # cards_gateway_client
         self.cards_gateway_client.issue_virtual_card(user_id=self.seed_user.user_id,
                                                      account_id=self.seed_user.debit_card_accounts[0].account_id)
-
-
 class IssueVirtualCardScenarioUser(LocustBaseUser):
     tasks = [IssueVirtualCardTaskSet]
